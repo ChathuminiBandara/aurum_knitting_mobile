e0ca68434eb487d0da0cca61ccf5fffcfed6023a
ChathuminiBandara <chathubandara500@gmail.com>
2025-02-18 08:36:52 +0530

    flow change / app -- _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
-import {Stack} from "expo-router";
+import { StatusBar } from "react-native";
+import { Stack } from "expo-router";
 
-export default function RootLayout(){
-    return(
-        <Stack screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Start"  options={{headerShown:false}}/>
-        </Stack>
+export default function Layout() {
+    return (
+        <>
+            {/* ✅ Show Status Bar */}
+            <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+
+            {/* ✅ Your Stack Navigation */}
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+            </Stack>
+        </>
     );
 }
-
